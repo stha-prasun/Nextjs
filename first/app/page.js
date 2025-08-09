0259cb9faaaf029672f9453fe2a773b5bffb1814
Prasun Shrestha <shresthaprasun88@gmail.com>
2025-08-09 13:01:22 +0545

    Next.js Basics

---
+++ first/app/page.js
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="bg-blue-200">Home Page</div>
+    </>
+  );
+};
+
+export default page;
